f611bb3839ef4e8954b1ce5dbb395f03c6aaf6c9
ThachK <kimmy.thach.asian@gmail.com>
2023-01-13 11:35:35 -0600

    Home Page from Trent :)

--- banking-app/src/pages/Home.tsx
+++ banking-app/src/pages/Home.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import "./Home.css";
 
 const Home: React.FC<any> = () => {
-	return <div>Home</div>;
+	return (
+		<div className="Home flex-column">
+			<div className="home-image">
+				<img src={require("../assets/rev_logo.png")} alt="revature_logo"></img>
+			</div>
+			<div className="home-container">
+				<h1 className="welcome-banner">
+					Thank you for choosing Revature Bank!
+				</h1>
+				<br />
+				<h3>Please Login or Register to Continue</h3>
+			</div>
+			<div className="home-animated">
+				<img
+					src={require("../assets/cred_cards2.gif")}
+					alt="animated_credit_cards"
+				></img>
+			</div>
+			<div className="marquee">
+				<h3 className="banner-text">
+					Start tracking your Revature Points today! Join our cashback reward
+					system! Track income and expenses!
+				</h3>
+				<h3 className="banner-text" style={{ left: 1200 }}>
+					Start tracking your Revature Points today! Join our cashback reward
+					system! Track income and expenses!
+				</h3>
+				<h3 className="banner-text" style={{ left: 2400 }}>
+					Start tracking your Revature Points today! Join our cashback reward
+					system! Track income and expenses!
+				</h3>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
